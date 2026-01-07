f50d66743b6d26820c238eebc75995f8aa9a5014
yurip120504-lgtm <yurip120504@gmail.com>
2026-01-07 16:45:24 +0900

    Update library_chatbot.py

--- library_chatbot.py
+++ library_chatbot.py
@@ -1,9 +1,9 @@
 import os
 import streamlit as st
-import nest_asyncio
+
 
 # Streamlit에서 비동기 작업을 위한 이벤트 루프 설정
-nest_asyncio.apply()
+
 
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
@@ -139,3 +139,4 @@ if prompt_message := st.chat_input("Your question"):
             with st.expander("참고 문서 확인"):
                 for doc in response['context']:
                     st.markdown(doc.metadata['source'], help=doc.page_content)
+
